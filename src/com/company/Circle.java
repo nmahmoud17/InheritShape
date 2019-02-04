22a0ac59ad619c243f6558a95db21838a178f802
NM17 <nada.mahmoud@detroitlabs.com>
2019-02-04 09:10:43 -0500

    getArea and getPerimeter methods

--- src/com/company/Circle.java
+++ src/com/company/Circle.java
@@ -8,4 +8,13 @@ public class Circle extends Shape {
         super (color, filled);
         this.radius = radius;
     }
+
+    public void getArea() {
+        double i = radius;
+        System.out.println(Math.pow(i, 2)* Math.PI);
+    }
+
+    public void getPerimeter() {
+        System.out.println(2*Math.PI*radius);
+    }
 }
